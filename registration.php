df405388edbafce2b0e9b5a5d78b8c326116ddff
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-31 22:15:59 +0800

    login page

---
+++ registration.php
@@ -0,0 +1,16 @@
+<div class="col-md-6">
+                    <h4>Don't have an account yet? Register!</h4>
+                    <form method="post" action="home.html">
+                    <div class="form-group">
+                        <input class="form-control" type="text" name="username" placeholder="Username">
+                    </div>
+
+                    <div class="form-group">
+                        <input class="form-control" type="text" name="location" placeholder="Location">
+                    </div>
+
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="password" placeholder="Password">
+                    </div>
+
+                    <div class="form-group">

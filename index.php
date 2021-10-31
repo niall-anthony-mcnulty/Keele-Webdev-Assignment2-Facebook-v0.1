bc747a691aeb19f02607d89488da8ccf5d4fad84
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-31 17:04:27 +0800

    starting code

--- index.php
+++ index.php
@@ -1,25 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>FaceClone</title>
-
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
+  <title>FriendZone</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css" type="text/css">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="icon" href="img/friendzonefavicon.png">
+  <link href="https://fonts.googleapis.com/css2?family=Sora:wght@800&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Bellota&family=Bellota+Text&display=swap" rel="stylesheet", type='text/css'>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  
 <body>
-  <!-- nav -->
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="index.php">FaceClone</a>
+  <nav class="navbar login">
+    <div class="container login-logo">
+      <div class='center'>
+        <div class='row justify-content-center no-gutters login-row'>
+           <div class ='col-xs-12 col-sm-10 col-md-8 col-lg-8 login-div'>
+              <a class="navbar-brand" href="index.php">
+                <img src='img/friendzonefavicon.png' alt='FriendZone Logo' class='login-img-center'>
+                <span class='title-login'> Friend<u>Z</u>one </span>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </nav>
-  <!-- ./nav -->
-
-  <!-- main -->
   <main class="container">
-  <h1 class="text-center">Welcome to FaceClone! <br><small>A simple Facebook clone.</small></h1>
+  <h1 class="text-center">Welcome to FriendZone!</h1>
 
     <div class="row">
       <div class="col-md-6">
@@ -75,7 +89,7 @@
     </ul>
   </footer>
   <!-- ./footer -->
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 </html>

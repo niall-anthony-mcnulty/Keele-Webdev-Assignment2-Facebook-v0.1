408a1d39f928af7a7f505719a1094883c44d517c
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-11-02 00:25:32 +0800

    larger mobiles

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>FriendZone login page</title>
+    <title>FriendZone Login</title>
     <meta name="description" content="login-page">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -23,7 +23,7 @@
                 <div class="container nav">
                     <div class='row justify-content-center no-gutters title-row w-100'>
                         <div class ='col-xs-12 col-sm-10 col-md-8 col-lg-8 title-col'>
-                            <a href='index.php' class='login-link'><img src='img/logo.png' alt='Friendzone logo' class='logo-pic'><span class='login-title'>Friend<u>Z</u>one</span></a>
+                            <a href='index.php' class='login-link'><img src='img/logo.png' alt='Friendzone logo' class='logo-pic registration-logo-pic'><span class='login-title'>Friend<u>Z</u>one</span></a>
                         </div>
                     </div>
                 </div>
@@ -33,7 +33,7 @@
                     <div class ='col-xs-11 col-sm-11 col-md-11 col-lg-11 main-col'>
                         <form id='login-form' method="post" action="home.php">
                             <div class="form-group">
-                                <input class="form-control" type="email" name="email" placeholder="enter@email">
+                                <input class="form-control" type="email" name="email" placeholder="Email">
                             </div>
 
                             <div class="form-group">

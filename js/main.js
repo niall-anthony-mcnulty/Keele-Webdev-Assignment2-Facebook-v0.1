05dff1cbf154432a0afa71a5323ad2fa48eaa365
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-11-04 00:32:35 +0800

    query correct

--- js/main.js
+++ js/main.js
@@ -22,40 +22,40 @@ $(document).ready(function() {
 
 });
     
-$(document).ready(function() {
+// $(document).ready(function() {
     
-    $('form#registration-form').submit(function(e) {
+//     $('form#registration-form').submit(function(e) {
 
-        var form = this;
+//         var form = this;
 
-        e.preventDefault();
+//         e.preventDefault();
 
 
-        setTimeout(function() {
+//         setTimeout(function() {
             
-            $('.registration-submit-form').hide();
+//             $('.registration-submit-form').hide();
 
-            setTimeout(function() {
+//             setTimeout(function() {
                 
-                $('.title-registration-col').after("<span class='success-msg'> Registration was successful!</span>");
-                $('.success-msg').css({'justify-content':'center','text-align':'center', 'margin-top':'20%'});
+//                 $('.title-registration-col').after("<span class='success-msg'> Registration was successful!</span>");
+//                 $('.success-msg').css({'justify-content':'center','text-align':'center', 'margin-top':'20%'});
             
-                  setTimeout(function() {
+//                   setTimeout(function() {
 
-                    form.submit();
-                    }, 4000);
+//                     form.submit();
+//                     }, 4000);
                
-            }, 500);
+//             }, 500);
         
         
-        }, 100);
+//         }, 100);
     
-    });
+//     });
 
         // $('.registration-form').hide();
         // $('.registration-col').append("<h1 class='successful-registration'> Registration was successful! Welcome to Friend<u>Z</u>one </h1>")
         // $('.successful-registration').delay(3000).remove()
         // $('.registration-form').show()
         
-})
+
 
--- registration-credentials.php
+++ registration-credentials.php
@@ -5,47 +5,50 @@
     require_once('includes/db-login.php');
 
     $fullName = $_POST['name'];
-    $userName = $_POST['username'];
+    $user = $_POST['userName'];
     $email = $_POST['Email'];
-    $password = $_POST['new-password'];
+    $password = password_hash($_POST['new-password'], PASSWORD_DEFAULT);
     $userType = $_POST['usertype'];
 
     
     // check no users have the same email or username 
 
     $sql1 = mysqli_query($conn, ("SELECT email FROM users WHERE email = $email"));
-    $sql2 = mysqli_query($conn, ("SELECT userName FROM users WHERE userName = $userName"));
+    $sql2 = mysqli_query($conn, ("SELECT userName FROM users WHERE userName = $user"));
 
 
 
     if ($sql1 == TRUE) {
 
         echo "<p> This email address is already in use! Please try another. </p>";
+        header("Location:registration.php");
+
+
     
     } 
     elseif ($sql2 == TRUE) {
 
         echo "<p> This username is already in use! Please try another. </p>";
+        header("Location:registration.php");
     }
 
     else {
             
     
     // if not, insert date into database table //
-        $sql = "INSERT INTO users (email, fullName, userName, userPassword, userType) VAlUES (?,?,?,?,?)";
-        $stmt = $conn->$prepare();
-        $stmt->bind_param("sssss", $email, $fullName, $userName, $password, $userType);
-        $stmt->execute();
+
+        $stmt = $conn->prepare("INSERT INTO users (email, fullName, userName, userPassword, userType) VAlUES (?,?,?,?,?)");
+        $stmt->bind_param("sssss", $email, $fullName, $user, $password, $userType);
         
         if ($stmt->execute()) {
         
             echo "<p class='success-msg'> Successfully added to DB </p>";
-            header("Location: ", 'index.php');
+            header("Location:index.php");
             
             }
             
             else { echo "<p class='fail-msg'> Failed </p>";
-                    header("Location:", 'registration.php');
+                    header("Location:registration.php");
        
              }
             

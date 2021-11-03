70f9188dd58dc4d662ee4a1593c3db8921577f9c
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-11-03 23:18:21 +0800

    sql reg

---
+++ registration-credentials.php
@@ -0,0 +1,50 @@
+<?php 
+
+    // connect to db //
+
+    require_once('includes/db-login.php');
+
+    $fullName = $_POST['name'];
+    $userName = $_POST['userName'];
+    $email = $_POST['Email'];
+    $password = $_POST['new-password'];
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    $userType = $_POST['usertype'];
+
+    // check no users have the same email or username 
+
+    $sql1 = mysqli_query($conn, ("SELECT email FROM users WHERE email = $email"));
+    $sql2 = mysqli_query($conn, ("SELECT userName FROM users WHERE userName = $userName"));
+
+
+
+    if ($sql1 == TRUE) {
+
+        echo "<p> This email address is already in use! Please try another. </p>";
+    
+    } 
+    elseif ($sql2 == TRUE) {
+
+        echo "<p> This username is already in use! Please try another. </p>";
+    }
+
+    else {
+            
+    
+    // if not, insert date into database table //
+        $stmt = $conn->$prepare("INSERT INTO users ('email', 'fullName', 'userName', 'userPassword', 'userType') VAlUES (?,?,?,?,?)");
+        $stmt->bind_param("sssss", $email, $fullName, $userName, $password, $userType);
+        // $stmt->execute();
+        
+        if ($stmt->execute()) {
+        
+            echo "<p class='success-msg'> Successfully added to DB </p>";
+            
+            }
+            
+            else { echo "<p class='fail-msg'> Failed </p>";
+             }
+       
+}
+
+?>

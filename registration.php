955d18f6868b21e31bd189c620e39268b800e8d7
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-11-15 20:06:02 +0800

    registration maxlengths

--- registration.php
+++ registration.php
@@ -36,20 +36,20 @@
                             <h1 class='signup-text'> Sign Up </h1>
                             <div class='name-wrapper'>
                                 <div class="registration-group first-name full-name">
-                                    <input class="signup-form" type="text" name="name" placeholder="Full name" required="true">
+                                    <input class="signup-form" type="text" name="name" placeholder="Full name" required="true" maxlength="50">
                                 </div>
                                 <div class="registration-group last-name username">
-                                    <input id ="user--name" class="signup-form" type="text" name="user--name" placeholder="Username" required='true'>
+                                    <input id ="user--name" class="signup-form" type="text" name="user--name" placeholder="Username" required='true' maxlength='50'>
                                 </div>
                             </div>
                             <div class="registration-group">
                             <!-- https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression email regex -->
-                                <input id ='user--email' class="signup-form" type='email' pattern="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|'(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*')@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])" name="user--email" placeholder="Email address" autocomplete="Email-Address" required='true'>
+                                <input id ='user--email' class="signup-form" type='email' pattern="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|'(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*')@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])" name="user--email" placeholder="Email address" autocomplete="Email-Address" required='true' maxlength='50'>
                             </div>
 
                             <div class="registration-group">
                             <!-- https://gist.github.com/ravibharathii/3975295 regex-->
-                                <input class="singup-form registration-password" type="password" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" title='A minimum of eight characters, at least one uppercase letter, one lowercase letter, one number and one special character is required:' name="new-password" placeholder= 'Set password' autocomplete= "new-password" required='true'>
+                                <input class="singup-form registration-password" type="password" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" title='A minimum of eight characters, at least one uppercase letter, one lowercase letter, one number and one special character is required:' name="new-password" placeholder= 'Set password' autocomplete= "new-password" required='true' maxlength='300'>
                             </div>
                             <div class='reader-poster-wrapper'>
                                 <div class="registration-group poster-button">

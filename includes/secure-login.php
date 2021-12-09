3b5ad20c10cc0f827ccb0ab6b2221fc025b31fef
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-12-10 01:40:47 +0800

    feed can delete given session user

--- includes/secure-login.php
+++ includes/secure-login.php
@@ -3,7 +3,7 @@
 session_start();
 // if session ip ,id ,login not valid or session is over 60 minutes ---->  end session.
 if ($_SESSION['id'] != session_id() || !isset($_SESSION['loggedin']) || !$_SESSION['loggedin'] || ($_SESSION['ip'] != $_SERVER['REMOTE_ADDR']) || ((time() - $_SESSION['loggin_time']) > 3600)) {     session_destroy();
-      header("Location: index.php");
+      header("Location: ../index.php");
       exit;
 }
 

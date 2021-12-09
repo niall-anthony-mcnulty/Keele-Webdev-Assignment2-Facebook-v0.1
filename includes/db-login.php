c0670dda6a10c92423f589d0596277e3a34a7393
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-12-10 00:47:24 +0800

    db heroku

--- includes/db-login.php
+++ includes/db-login.php
@@ -3,7 +3,7 @@
 $hn = '127.0.0.1';
 $un = 'root';
 $pw = '';
-$db = 'Database operationsheroku_702d464ce46caa4';
+$db = 'heroku_702d464ce46caa4';
 $conn = new mysqli($hn, $un, $pw, $db);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());

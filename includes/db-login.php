b08bbb006cd2302ea6c3934c6bc0f42480cc62dd
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-12-10 00:53:57 +0800

    both db options

--- includes/db-login.php
+++ includes/db-login.php
@@ -1,10 +1,23 @@
 <?php // login.php
 
-$hn = '127.0.0.1';
-$un = 'root';
-$pw = '';
-$db = 'heroku_702d464ce46caa4';
-$conn = new mysqli($hn, $un, $pw, $db);
+//Get Heroku ClearDB connection information
+
+
+$cleardb_server   = 'eu-cdbr-west-01.cleardb.com';
+$cleardb_username = 'b260447ff86cf8';
+$cleardb_password = 'a61f5c66';
+$cleardb_db       = 'heroku_702d464ce46caa4';
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+// $hn = '127.0.0.1';
+// $un = 'staff';
+// $pw = 'admin123';
+// $db = 'WEATHER';
+// $conn = new mysqli($hn, $un, $pw, $db);
+
+$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }

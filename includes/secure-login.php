15de29363d50de822541acac545af9f6e11342d2
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-12-10 16:50:53 +0800

    sesh ip

--- includes/secure-login.php
+++ includes/secure-login.php
@@ -5,16 +5,14 @@ session_start();
 
 // get client ip from heroku as they pass through proxies
 function getIpAddress() {
-      // check for ip via http_x_forwarded
       if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
           $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
           return trim(end($ipAddresses));
       }
-      // else get remote addr
       else {
           return $_SERVER['REMOTE_ADDR'];
       }
-  };
+  }
 
 
 if ($_SESSION['id'] != session_id() || !isset($_SESSION['loggedin']) || !$_SESSION['loggedin'] || ($_SESSION['ip'] != getIpAddress()) || ((time() - $_SESSION['loggin_time']) > 3600)) {     

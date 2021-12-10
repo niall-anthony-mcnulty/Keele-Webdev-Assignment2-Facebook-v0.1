550929b4592547b4bb173ec8b9247a9dda5d437c
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-12-10 15:34:37 +0800

    ip sesh

--- includes/secure-login.php
+++ includes/secure-login.php
@@ -3,13 +3,7 @@
 session_start();
 // if session ip ,id ,login not valid or session is over 60 minutes ---->  end session.
 
-
-if ($_SESSION['id'] != session_id() || !isset($_SESSION['loggedin']) || !$_SESSION['loggedin'] || ($_SESSION['ip'] != getIpAddress()) || ((time() - $_SESSION['loggin_time']) > 3600)) {     
-      session_destroy();
-      header("Location: ../index.php");
-      exit;
-}
-
+// get client ip from heroku as they pass through proxies
 function getIpAddress() {
       if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
           $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
@@ -19,6 +13,12 @@ function getIpAddress() {
           return $_SERVER['REMOTE_ADDR'];
       }
   };
+
+if ($_SESSION['id'] != session_id() || !isset($_SESSION['loggedin']) || !$_SESSION['loggedin'] || $_SESSION['ip'] != getIpAddress() || ((time() - $_SESSION['loggin_time']) > 3600)) {     
+      session_destroy();
+      header("Location: ../index.php");
+      exit;
+}
 
 
 ?>

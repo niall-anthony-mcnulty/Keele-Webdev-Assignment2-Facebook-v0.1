a96efd11bd85689cfd20b9ba48287b67253f5727
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-12-10 15:20:27 +0800

    heroku sessions

--- includes/secure-login.php
+++ includes/secure-login.php
@@ -3,7 +3,7 @@
 session_start();
 // if session ip ,id ,login not valid or session is over 60 minutes ---->  end session.
 
-if ($_SESSION['id'] != session_id() || !isset($_SESSION['loggedin']) || !$_SESSION['loggedin'] || ($_SESSION['ip'] != $_SERVER['HTTP_X_FORWARD_FOR']) || ((time() - $_SESSION['loggin_time']) > 3600)) {     
+if ($_SESSION['id'] != session_id() || !isset($_SESSION['loggedin']) || !$_SESSION['loggedin'] || ($_SESSION['ip'] != $_SERVER['HTTP_X_FORWARDED_FOR']) || ((time() - $_SESSION['loggin_time']) > 3600)) {     
       session_destroy();
       header("Location: ../index.php");
       exit;

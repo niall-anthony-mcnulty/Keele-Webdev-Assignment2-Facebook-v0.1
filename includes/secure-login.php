d223a9e3c0aca55f93c07546a4d0834e254af51e
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-12-10 15:25:43 +0800

    function for getting ip address for sessions

--- includes/secure-login.php
+++ includes/secure-login.php
@@ -3,10 +3,21 @@
 session_start();
 // if session ip ,id ,login not valid or session is over 60 minutes ---->  end session.
 
-if ($_SESSION['id'] != session_id() || !isset($_SESSION['loggedin']) || !$_SESSION['loggedin'] || ($_SESSION['ip'] != $_SERVER['HTTP_X_FORWARDED_FOR']) || ((time() - $_SESSION['loggin_time']) > 3600)) {     
+function getIpAddress() {
+      if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+          $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+          return trim(end($ipAddresses));
+      }
+      else {
+          return $_SERVER['REMOTE_ADDR'];
+      }
+  };
+
+if ($_SESSION['id'] != session_id() || !isset($_SESSION['loggedin']) || !$_SESSION['loggedin'] || ($_SESSION['ip'] != getIpAddress()) || ((time() - $_SESSION['loggin_time']) > 3600)) {     
       session_destroy();
       header("Location: ../index.php");
       exit;
 }
+
 
 ?>
